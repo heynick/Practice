7f1475924c6ee277659443363bc89cc4517ee905
hey.nick <hey.nick@outlook.com>
2022-04-06 16:01:47 +0800

    update
    Signed-off-by: hey.nick <hey.nick@outlook.com>

---
+++ src/main/java/Solution420.java
@@ -0,0 +1,18 @@
+/**
+ * @Author HEYNICK
+ * @Date 2022/4/2 14:32
+ * @Description 强密码检验器
+ */
+public class Solution420 {
+    public static void main(String[] args) {
+        String password = "aA1";
+        Solution420 solution420 = new Solution420();
+        int res = solution420.strongPasswordChecker(password);
+        System.out.println(res);
+    }
+
+    public int strongPasswordChecker(String password) {
+
+
+    }
+}
